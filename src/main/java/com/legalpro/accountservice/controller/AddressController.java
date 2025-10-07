a172508d0b7821534736b104833352ac6088fa1a
Springboot2025 <legalpro42@gmail.com>
2025-10-07 06:15:15 +0530

    address autocomplete

--- src/main/java/com/legalpro/accountservice/controller/AddressController.java
+++ src/main/java/com/legalpro/accountservice/controller/AddressController.java
@@ -1,9 +1,11 @@
 package com.legalpro.accountservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/address")
@@ -16,12 +18,32 @@ public class AddressController {
 
     @GetMapping("/autocomplete")
     public ResponseEntity<String> autocomplete(@RequestParam String input) {
-        String url = "https://maps.googleapis.com/maps/api/place/autocomplete/json"
-                + "?input=" + input
-                + "&types=address"
-                + "&key=" + googleApiKey;
+        String url = "https://places.googleapis.com/v1/places:autocomplete";
 
-        String response = restTemplate.getForObject(url, String.class);
-        return ResponseEntity.ok(response);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-Goog-Api-Key", googleApiKey);
+        headers.set("X-Goog-FieldMask", "suggestions.placePrediction.placeId,suggestions.placePrediction.text.text");
+
+        Map<String, Object> requestBody = Map.of("input", input);
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+        return ResponseEntity.ok(response.getBody());
+    }
+
+    @GetMapping("/details")
+    public ResponseEntity<String> getPlaceDetails(@RequestParam String placeId) {
+        String url = "https://places.googleapis.com/v1/places/" + placeId
+                + "?fields=formattedAddress,addressComponents,location";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Goog-Api-Key", googleApiKey);
+        headers.set("X-Goog-FieldMask", "formattedAddress,addressComponents,location");
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        return ResponseEntity.ok(response.getBody());
     }
 }

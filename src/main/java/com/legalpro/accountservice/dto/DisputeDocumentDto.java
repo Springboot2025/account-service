9ea4a8f21d3094e05b790dd2fb52cf85608647c4
Springboot2025 <legalpro42@gmail.com>
2025-11-11 21:32:14 +0530

    DTO error

--- src/main/java/com/legalpro/accountservice/dto/DisputeDocumentDto.java
+++ src/main/java/com/legalpro/accountservice/dto/DisputeDocumentDto.java
@@ -1,13 +1,17 @@
 package com.legalpro.accountservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DisputeDocumentDto {
 
     private Long id;
--- src/main/java/com/legalpro/accountservice/dto/DisputeDto.java
+++ src/main/java/com/legalpro/accountservice/dto/DisputeDto.java
@@ -1,7 +1,9 @@
 package com.legalpro.accountservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,6 +11,8 @@ import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DisputeDto {
 
     private Long id;

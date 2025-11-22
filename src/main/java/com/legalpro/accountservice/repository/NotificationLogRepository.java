a6e6eebe46379c0ea736187bd29b2178199b0a43
Springboot2025 <legalpro42@gmail.com>
2025-11-22 14:49:10 +0530

    Notification logs

--- src/main/java/com/legalpro/accountservice/repository/NotificationLogRepository.java
+++ src/main/java/com/legalpro/accountservice/repository/NotificationLogRepository.java
@@ -10,18 +10,12 @@ import java.util.List;
 import java.util.UUID;
 
 public interface NotificationLogRepository extends JpaRepository<NotificationLog, Long> {
+
     List<NotificationLog> findByUserUuidOrderBySentAtDesc(UUID userUuid);
+
     @Modifying
     @Query("UPDATE NotificationLog n SET n.isRead = true WHERE n.userUuid = :userUuid")
     void markAllAsReadByUser(@Param("userUuid") UUID userUuid);
-
-    @Modifying
-    @Query("UPDATE NotificationLog n SET n.deletedAt = CURRENT_TIMESTAMP WHERE n.id = :id")
-    void softDeleteById(@Param("id") Long id);
-
-    @Modifying
-    @Query("UPDATE NotificationLog n SET n.deletedAt = CURRENT_TIMESTAMP WHERE n.userUuid = :userUuid")
-    void softDeleteByUser(@Param("userUuid") UUID userUuid);
 
     boolean existsByIdAndUserUuid(Long id, UUID userUuid);
 
@@ -32,5 +26,4 @@ public interface NotificationLogRepository extends JpaRepository<NotificationLog
     @Modifying
     @Query("DELETE FROM NotificationLog n WHERE n.userUuid = :userUuid")
     void deleteAllByUserUuid(@Param("userUuid") UUID userUuid);
-
 }
